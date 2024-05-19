edfdd0a8afbe93073a793ab52733390748903b63
Gauravelp <gaurav@elphill.com>
2024-05-19 17:30:48 +0000

    container congig save

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -16,6 +16,8 @@ console.log(accountId);
 /*
 Prefer not use var
 because of issue in block scope and functional scope
+
+{} -> this called scope
 */
 
 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(); // we are using nodejs, not browser
+
+//readability
+
+console.log(3+3); console.log("Hi");  //not a good practice
+
+
+/* document for js
+ site -> mdn javascript
+ site -> tc39.es/ecma262/
+*/
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/*
+number => 2 to power 53
+bigint
+string =>
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+object
+*/
+
+console.log(typeof age);
