7b6b90ae51ba5ce26e2c0399277bece9fc9f1079
Gauravelp <gaurav@elphill.com>
2024-05-23 03:48:56 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,6 @@
 // array
 
-const myArr = [0, 5, 9, 4]
+const myArr = [0, 5, 9, 4, 8, 9]
 const myHeros = ["shaktiman", "natraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
@@ -33,3 +33,9 @@ console.log("A", myArr);
 const myn1 = myArr.slice(1, 3)
 
 console.log(myn1);
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
