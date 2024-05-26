ef4b4311151a160946919f9626df9911eb9d93bf
Gauravelp <gaurav@elphill.com>
2024-05-26 07:30:21 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+//  singleton
+
+// object literals
+
+// 1st method to create oj 
+// object.create
+
+// 2nd method
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Hitman",
+    age: 18,
+    [mySym]: "mykey1",
+    "full name": "Hitman Das",  //when such frormat then calling through JsUser.email dont work
+    location: "kolkat",
+    email: "absc@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saurday"] 
+}
+
+// mostly in arrays
+// myarray = ["h", "i"]
+// myArray[2] or [0] etc..
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+
+console.log(JsUser[mySym]);
+// console.log(typeof JsUser.mySym);
+// console.log(typeof JsUser[mySym]);
+
+//object ffreeze
+
+JsUser.email = "Hitman@ggmmail.com"
+Object.freeze(JsUser)
+JsUser.email = "Hitman@kkhhyyrr.com"
+
+console.log(JsUser);
