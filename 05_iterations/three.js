4875d20f917162ba9f12cd5a56b110593e16d669
Gauravelp <gaurav@elphill.com>
2024-06-04 16:40:17 +0000

    loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,26 @@
+//// for of loop
+
+// ["", "",""]
+
+//[{}, {}, {}]
+
+const  arr = [1, 2, 4, 5, 6 ]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello world!"
+
+for (const gre of greetings) {
+    // console.log(`Each char is ${gre}`);
+}
+
+////  Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('FR', "France")
+
+console.log(map);
