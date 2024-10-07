b2cf23d230bb32f46da4e9d001ce0810e05b6511
Gauravelp <gaurav@elphill.com>
2024-10-07 03:11:05 +0000

    revision

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -6,12 +6,12 @@ const balance = new Number(100)
 // console.log(balance);
 
 // console.log(balance.toString());
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));  //value after decimal like 100.00
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));  //value after decimal like 100.00
 
 const otherNumber = 123.8769
 
-// console.log(otherNumber.toPrecision(3));  //to next /round off value
+console.log(otherNumber.toPrecision(3));  //to next /round off value
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString()); //us standards
@@ -29,11 +29,11 @@ const hundreds = 1000000
 // console.log(Math.min(4,5,7,9));
 // console.log(Math.max(4,5,7,9));
 
-console.log(Math.random())  //value btwn 0-1
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random())  //value btwn 0-1
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
